3e46d8bf5e0e679031f345e00db65d0f2a8273cd
shaoziyang <shaoziyang@126.com>
2019-06-13 00:28:28 +0800

    add Events
    add Key Pressed and Released Event
    add Key Name

--- sh01.ts
+++ sh01.ts
@@ -2,6 +2,25 @@
  * XinaBox SH01 extension for makecode
  */
 
+declare enum SH01_KEY {
+    //% block="UP"
+    KEY_UP = 1,
+    //% block="DOWN"
+    KEY_DOWN = 8,
+    //% block="LEFT"
+    KEY_LEFT = 16,
+    //% block="RIGHT"
+    KEY_RIGHT = 32,
+    //% block="TRIANGLE"
+    KEY_TRI = 1,
+    //% block="NO"
+    KEY_NO = 8,
+    //% block="SQUARE"
+    KEY_SQUARE = 16,
+    //% block="CIRCLE"
+    KEY_CIRCLE = 32,
+}
+
 /**
  * SH01 block
  */
@@ -10,6 +29,8 @@ namespace SH01 {
     let CAP1296_I2C_ADDRESS = 40
     let REG_MainControl = 0
     let REG_InputStatus = 3
+    let KeyPressed = false
+    let KeyReleased = true
 
     function setreg(reg: number, dat: number): void {
         let buf = pins.createBuffer(2);
@@ -24,29 +45,122 @@ namespace SH01 {
     }
 
     /**
+     * Key Pressed Event
+     */
+    //% block="Key Pressed %key"
+    export function onKeyPressed(key: SH01_KEY, body: () => void): void {
+        control.inBackground(function () {
+            while (true) {
+                if (read() == key) {
+                    if (!KeyPressed) {
+                        KeyPressed = true
+                        body()
+                    }
+                }
+                else KeyPressed = false
+                basic.pause(100)
+            }
+        })
+    }
+
+    /**
+     * Key Released Event
+     */
+    //% block="Key Released %key"
+    export function onKeyReleased(key: SH01_KEY, body: () => void): void {
+        control.inBackground(function () {
+            while (true) {
+                if (read() == key) {
+                    KeyReleased = false
+                }
+                else {
+                    if (!KeyReleased) {
+                        KeyReleased = true
+                        body()
+                    }
+                }
+                basic.pause(100)
+            }
+        })
+    }
+
+    /**
+     * TRIANGLE KEY
+     */
+    //% block="TRIANGLE" advanced=true
+    export function KEY_TRI(): SH01_KEY {
+        return SH01_KEY.KEY_UP
+    }
+
+    /**
+     * NO KEY
+     */
+    //% block="NO" advanced=true
+    export function KEY_NO(): SH01_KEY {
+        return SH01_KEY.KEY_DOWN
+    }
+
+    /**
+     * SQUARE KEY
+     */
+    //% block="SQUARE" advanced=true
+    export function KEY_SQUARE(): SH01_KEY {
+        return SH01_KEY.KEY_LEFT
+    }
+
+    /**
+     * CIRCLE KEY
+     */
+    //% block="CIRCLE" advanced=true
+    export function KEY_CIRCLE(): SH01_KEY {
+        return SH01_KEY.KEY_RIGHT
+    }
+
+    /**
+     * UP KEY
+     */
+    //% block="UP" advanced=true
+    export function KEY_UP(): SH01_KEY {
+        return SH01_KEY.KEY_UP
+    }
+
+    /**
+     * DOWN KEY
+     */
+    //% block="DOWN" advanced=true
+    export function KEY_DOWN(): SH01_KEY {
+        return SH01_KEY.KEY_DOWN
+    }
+
+    /**
+     * LEFT KEY
+     */
+    //% block="LEFT" advanced=true
+    export function KEY_LEFT(): SH01_KEY {
+        return SH01_KEY.KEY_LEFT
+    }
+
+    /**
+     * RIGHT KEY
+     */
+    //% block="RIGHT" advanced=true
+    export function KEY_RIGHT(): SH01_KEY {
+        return SH01_KEY.KEY_RIGHT
+    }
+
+    /**
      * read touch key, if a touch key pressed, return 1-4.
      * If no touch key pressed, return 0.
      */
-    //% blockId="SH01_READ" block="Read Touch"
-    //% weight=80 blockGap=8
-    export function read(): number {
+    //% block="Read Touch"
+    //% weight=80
+    export function read(): SH01_KEY {
         let t = getreg(REG_InputStatus)
 
         if (t > 0) {
             setreg(REG_MainControl, 0)
         }
-        switch (t) {
-            case 1:
-                return 1
-            case 8:
-                return 2
-            case 16:
-                return 3
-            case 32:
-                return 4
-            default:
-                return 0
-        }
+        return t
     }
 
     read()
